5dbe49ec264d8a911ef71e744d66bc495ed4537b
AlexanderBeichuk <alexander.beichuk@gmail.com>
2018-05-01 21:18:48 +0300

    added API request for Locations list

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,36 +1,44 @@
-module.exports.homeList = function(req, res){
-    res.render('location-list', {
-        title: 'Home',
-        pageHeader: {
-            title: 'Loc8r',
-            strapline: 'Find place to work with WiFi near you!'
-        },
-				sidebar: 'Looking for Wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps With coffee, cake or a pit? Let Loc8r help you find the place you\'re looking for.',
-        locations: [
-            {
-                name: 'StarCups',
-                address: '125 High Street, Reading, RG6 1PS',
-                rating: 3,
-                facilities: ['Hot drinks', 'Food', 'Premium WiFi'],
-                distance: '100 m'
-            },
-            {
-                name: 'Cafe Hero',
-                address: '125 High Street, Reading, RG6 1PS',
-                rating: 4,
-                facilities: ['Hot drinks', 'Food', 'Premium WiFi'],
-                distance: '200 m'
-            },
-            {
-                name: 'Burger Queen',
-                address: '125 High Street, Reading, RG6 1PS',
-                rating: 2,
-                facilities: ['Food', 'Premium WiFi'],
-                distance: '250 m'
-            }
-        ]
-    })
+var request = require('request');
+
+module.exports = {
+	homeList: homeList,
+
 };
+
+var apiOptions = {
+	server : "http://localhost:3000"
+};
+if (process.env.NODE_ENV === 'production') {
+	apiOptions.server = "https://getting-mean-loc8r.herokuapp.com";
+}
+
+function homeList (req, res){
+	var requestOptions, path;
+	path = '/api/locations';
+	requestOptions = {
+		url : apiOptions.server + path,
+		method : "GET",
+		json : {},
+		qs : {
+			lng : -0.7992599,
+			lat : 51.378091,
+			maxDistance : 20
+		}
+	};
+	request(
+		requestOptions,
+		function(err, response, body) {
+			var i, data;
+			data = body;
+			if (response.statusCode === 200 && data.length) {
+				for (i=0; i<data.length; i++) {
+					data[i].distance = _formatDistance(data[i].distance);
+				}
+			}
+			renderHomepage(req, res, data);
+		}
+	);
+}
 
 module.exports.locationsInfo = function(req, res){
 	res.render('location-info', {
@@ -89,3 +97,41 @@ module.exports.addReview = function(req, res){
     		title: 'Review StarCups'
 			}})
 };
+
+function renderHomepage (req, res, responseBody){
+	var message;
+	if (!(responseBody instanceof Array)) {
+		message = "API lookup error";
+		responseBody = [];
+	} else {
+		if (!responseBody.length) {
+			message = "No places found nearby";
+		}
+	}
+	res.render('locations-list', {
+		title: 'Loc8r - find a place to work with wifi',
+		pageHeader: {
+			title: 'Loc8r',
+			strapline: 'Find places to work with wifi near you!'
+		},
+		sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
+		locations: responseBody,
+		message: message
+	});
+}
+
+function _formatDistance (distance) {
+	var numDistance, unit;
+	if (distance && _isNumeric(distance)) {
+		if (distance > 1) {
+			numDistance = parseFloat(distance).toFixed(1);
+			unit = 'km';
+		} else {
+			numDistance = parseInt(distance * 1000,10);
+			unit = 'm';
+		}
+		return numDistance + unit;
+	} else {
+		return "?";
+	}
+}
